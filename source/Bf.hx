e46ee019bebf403cbb7d07aea35ec12b6fff7dba
Hhhxdjejjd <99557090+Hhhxdjejjd@users.noreply.github.com>
2022-04-18 16:30:12 -0400

    Dbbdbdbd
    Dgrgdgdg

--- source/Bf.hx
+++ source/Bf.hx
@@ -6,6 +6,6 @@ import flixel.FlxSprite;
 class Bf extends FlxSprite {
   public function new(x:Int = 0, y:Int = 0) {
     super(x, y);
-    loadGraphic("assets/images/BOYFRIEND.png", true, 32, 48);
+    loadGraphic("assets/images/BOYFRIEND.png", true, 100, 100);
   }
 }
