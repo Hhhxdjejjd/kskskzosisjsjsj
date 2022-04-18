02250d75084c1d455b3d62619f60c0a6c3925dca
Hhhxdjejjd <99557090+Hhhxdjejjd@users.noreply.github.com>
2022-04-18 15:49:55 -0400

    Xbdbgd
    Ddvvrvd

--- source/bf.hx
+++ source/bf.hx
@@ -4,5 +4,7 @@ import flixel.FlxG;
 import flixel.FlxSprite;
 
 class bf extends FlxSprite {
-  public function new(x:Int = 0, y:Int = 0)
+  public function new(x:Int = 0, y:Int = 0) {
+    super(x, y);
+  {
 }
