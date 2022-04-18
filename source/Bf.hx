c44e682badee9a475e20b16e3a2d07cb39e766a5
Hhhxdjejjd <99557090+Hhhxdjejjd@users.noreply.github.com>
2022-04-18 16:12:15 -0400

    Dbjdbdbd
    Dgdvdggr

--- source/Bf.hx
+++ source/Bf.hx
@@ -3,7 +3,7 @@ package;
 import flixel.FlxG;
 import flixel.FlxSprite;
 
-class bf extends FlxSprite {
+class Bf extends FlxSprite {
   public function new(x:Int = 0, y:Int = 0) {
     super(x, y);
     loadGraphic("assets/images/BOYFRIEND.png", true, 32, 48);
